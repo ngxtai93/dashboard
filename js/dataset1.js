8161a5e249dc4ff6ad55802a786c17f5ed41346f
hatimsdhuliawala <hatim.dhuliawala@gmail.com>
2017-04-05 21:33:03 -0500

    4th Dataset Complete

--- js/dataset1.js
+++ js/dataset1.js
@@ -1,5 +1,5 @@
 
- rederDataset3(datasetName, parentDiv) {
+ rederDataset4(datasetName, parentDiv) {
     DataFrame.fromCSV('datasets/'+datasetName+'.csv').then(
         df => {
           //chart()
@@ -8,85 +8,55 @@
                           //console.log(groupedDF.aggregate(group => group.count()));
 
              //console.log(cleanDF.listColumns());
-             const selectColumns = df.select('School ID','School Name','Street Address','City','State','ZIP','Phone Number',
-											 'Website','Blue Ribbon Award','CPS Performance Policy Level',
-											 'CPS Performance Policy Status','Probation Length','Overall Rating',
-											 'Student Response Rate','Teacher Response Rate','Involved Family',
-											 'Supportive Environment','Ambitious Instruction','Effective Leaders',
-											 'Collaborative Teachers','Safety','School Community',
-											 'Parent-Teacher Partnership','Quality of Facilities',
-											 'Healthy Schools Certification','Creative Schools Certification',
-											 'College Enrollment Rate Percentage 2013',
-											 'College Enrollment Rate Percentage 2012',
-											 'College Persistence Rate Percentage 2013',
-											 'College Persistence Rate Percentage 2012',
-											 'Student Attendance Percentage 2013','Student Attendance Percentage 2012',
-											 'One-Year DropOut Rate Percentage 2013','One-Year DropOut Rate Percentage 2012',
-											 'X Coordinate','Y Coordinate','Longitude','Latitude','Location');
+             const selectColumns = df.select('DBN','DISTRICT','SCHOOL','PRINCIPAL','PROGRESS REPORT TYPE','SCHOOL LEVEL',
+             								 'PEER INDEX','2011-2012 OVERALL GRADE','2011-2012 OVERALL SCORE',
+             								 '2011-12 OVERALL PERCENTILE','2011-2012 PROGRESS CATEGORY SCORE',
+             								 '2011-2012 PROGRESS GRADE','2011-2012 PERFORMANCE CATEGORY SCORE',
+             								 '2011-2012 PERFORMANCE GRADE','2011-2012 ENVIRONMENT CATEGORY SCORE',
+             								 '2011-2012 ENVIRONMENT GRADE','2011-2012 COLLEGE AND CAREER READINESS SCORE',
+             								 '2011-2012 COLLEGE AND CAREER READINESS GRADE','2011-2012 ADDITIONAL CREDIT',
+             								 '2010-11 PROGRESS REPORT GRADE','2009-10 PROGRESS REPORT GRADE');
 
-                dataset3Data = selectColumns.toCollection();
+                dataset4Data = selectColumns.toCollection();
                  //console.log(test);
                  var thead = '<tr>'+
-                               '<th>School ID</th><th>School Name</th><th>Street Address</th><th>City</th><th>State</th>'+	
-							   '<th>ZIP</th><th>Phone Number</th><th>Website</th><th>Blue Ribbon Award</th>'+
-							   '<th>CPS Performance Policy Level</th><th>CPS Performance Policy Status</th>'+
-							   '<th>Probation Length</th><th>Overall Rating</th><th>Student Response Rate</th>'+
-							   '<th>Teacher Response Rate</th><th>Involved Family</th><th>Supportive Environment</th>'+
-							   '<th>Ambitious Instruction</th><th>Effective Leaders</th><th>Collaborative Teachers</th>'+
-							   '<th>Safety</th><th>School Community</th><th>Parent-Teacher Partnership</th>'+
-							   '<th>Quality of Facilities</th><th>Healthy Schools Certification</th>'+
-							   '<th>Creative Schools Certification</th><th>College Enrollment Rate Percentage 2013</th>'+
-							   '<th>College Enrollment Rate Percentage 2012</th>'+
-							   '<th>College Persistence Rate Percentage 2013</th>'+
-							   '<th>College Persistence Rate Percentage 2012</th><th>Student Attendance Percentage 2013</th>'+
-							   '<th>Student Attendance Percentage 2012</th><th>One-Year DropOut Rate Percentage 2013</th>'+
-							   '<th>One-Year DropOut Rate Percentage 2012</th><th>X Coordinate</th><th>Y Coordinate</th>'+
-							   '<th>Longitude</th><th>Latitude</th><th>Location</th>';
+                 				'<th>DBN</th><th>DISTRICT</th><th>SCHOOL</th><th>PRINCIPAL</th>'+
+                 				'<th>PROGRESS REPORT TYPE</th><th>SCHOOL LEVEL</th><th>PEER INDEX</th>'+
+                 				'<th>2011-2012 OVERALL GRADE</th><th>2011-2012 OVERALL SCORE</th>'+
+                 				'<th>2011-12 OVERALL PERCENTILE</th><th>2011-2012 PROGRESS CATEGORY SCORE</th>'+
+                 				'<th>2011-2012 PROGRESS GRADE</th><th>2011-2012 PERFORMANCE CATEGORY SCORE</th>'+
+                 				'<th>2011-2012 PERFORMANCE GRADE</th><th>2011-2012 ENVIRONMENT CATEGORY SCORE</th>'+
+                 				'<th>2011-2012 ENVIRONMENT GRADE</th><th>2011-2012 COLLEGE AND CAREER READINESS SCORE</th>'+
+                 				'<th>2011-2012 COLLEGE AND CAREER READINESS GRADE</th>'+
+                 				'<th>2011-2012 ADDITIONAL CREDIT</th><th>2010-11 PROGRESS REPORT GRADE</th>'+
+                 				'<th>2009-10 PROGRESS REPORT GRADE</th>';
                                '</tr>';
                   $("#"+parentDiv+"Table thead").html(thead);
                   var tbody = $("#"+parentDiv+"Table tbody");
 
-            for ( var i = 0; i < dataset3Data.length; i++) {
+            for ( var i = 0; i < dataset4Data.length; i++) {
               var content = '<tr>'+
-                              '<td>'+dataset3Data[i]["School ID"]+'</td>'+
-                              '<td>'+dataset3Data[i]["School Name"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Street Address"]+'</td>'+
-                              '<td>'+dataset3Data[i]["City"]+'</td>'+
-                              '<td>'+dataset3Data[i]["State"]+'</td>'+
-                              '<td>'+dataset3Data[i]["ZIP"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Phone Number"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Website"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Blue Ribbon Award"]+'</td>'+
-                              '<td>'+dataset3Data[i]["CPS Performance Policy Level"]+'</td>'+
-                              '<td>'+dataset3Data[i]["CPS Performance Policy Status"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Probation Length"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Overall Rating"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Student Response Rate"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Teacher Response Rate"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Involved Family"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Supportive Environment"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Ambitious Instruction"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Effective Leaders"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Collaborative Teachers"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Safety"]+'</td>'+
-                              '<td>'+dataset3Data[i]["School Community"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Parent-Teacher Partnership"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Quality of Facilities"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Healthy Schools Certification"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Creative Schools Certification"]+'</td>'+
-                              '<td>'+dataset3Data[i]["College Enrollment Rate Percentage 2013"]+'</td>'+
-                              '<td>'+dataset3Data[i]["College Enrollment Rate Percentage 2012"]+'</td>'+
-                              '<td>'+dataset3Data[i]["College Persistence Rate Percentage 2013"]+'</td>'+
-                              '<td>'+dataset3Data[i]["College Persistence Rate Percentage 2012"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Student Attendance Percentage 2013"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Student Attendance Percentage 2012"]+'</td>'+
-                              '<td>'+dataset3Data[i]["One-Year DropOut Rate Percentage 2013"]+'</td>'+
-                              '<td>'+dataset3Data[i]["One-Year DropOut Rate Percentage 2012"]+'</td>'+
-                              '<td>'+dataset3Data[i]["X Coordinate"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Y Coordinate"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Longitude"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Latitude"]+'</td>'+
-                              '<td>'+dataset3Data[i]["Location"]+'</td>'+
+                              '<td>'+dataset4Data[i]["DBN"]+'</td>'+
+                              '<td>'+dataset4Data[i]["DISTRICT"]+'</td>'+
+                              '<td>'+dataset4Data[i]["SCHOOL"]+'</td>'+
+                              '<td>'+dataset4Data[i]["PRINCIPAL"]+'</td>'+
+                              '<td>'+dataset4Data[i]["PROGRESS REPORT TYPE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["SCHOOL LEVEL"]+'</td>'+
+                              '<td>'+dataset4Data[i]["PEER INDEX"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 OVERALL GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 OVERALL SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-12 OVERALL PERCENTILE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PROGRESS CATEGORY SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PROGRESS GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PERFORMANCE CATEGORY SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PERFORMANCE GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 ENVIRONMENT CATEGORY SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 ENVIRONMENT GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 COLLEGE AND CAREER READINESS SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 COLLEGE AND CAREER READINESS GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 ADDITIONAL CREDIT"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2010-11 PROGRESS REPORT GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2009-10 PROGRESS REPORT GRADE"]+'</td>'+
                             '</tr>';
               $(tbody).append(content);
             }
@@ -100,6 +70,35 @@
 
 
 
+
+
+
+
+
+
+
+
+DBN
+DISTRICT
+SCHOOL
+PRINCIPAL
+PROGRESS REPORT TYPE
+SCHOOL LEVEL
+PEER INDEX
+2011-2012 OVERALL GRADE
+2011-2012 OVERALL SCORE
+2011-12 OVERALL PERCENTILE
+2011-2012 PROGRESS CATEGORY SCORE
+2011-2012 PROGRESS GRADE
+2011-2012 PERFORMANCE CATEGORY SCORE
+2011-2012 PERFORMANCE GRADE
+2011-2012 ENVIRONMENT CATEGORY SCORE
+2011-2012 ENVIRONMENT GRADE
+2011-2012 COLLEGE AND CAREER READINESS SCORE
+2011-2012 COLLEGE AND CAREER READINESS GRADE
+2011-2012 ADDITIONAL CREDIT
+2010-11 PROGRESS REPORT GRADE
+2009-10 PROGRESS REPORT GRADE
 
 
 
--- js/extractData.js
+++ js/extractData.js
@@ -58,7 +58,21 @@ class Dataset3 extends Datasets {
     test.rederDataset3(datasetName, parentDiv);
   }
 
+
 }
+class Dataset4 extends Datasets {
+  constructor(datasetName, parentDiv) {
+    super();
+    this.datasetName=datasetName;
+    this.parentDiv=parentDiv;
+    var test = new renderDatasets(datasetName, parentDiv);
+    test.rederDataset4(datasetName, parentDiv);
+  }
+
+
+}
+
+
 class renderDatasets {
   constructor(datasetName, parentDiv) {
     this.datasetName=datasetName;
@@ -294,6 +308,70 @@ class renderDatasets {
                               '<td>'+dataset3Data[i]["Longitude"]+'</td>'+
                               '<td>'+dataset3Data[i]["Latitude"]+'</td>'+
                               '<td>'+dataset3Data[i]["Location"]+'</td>'+
+                            '</tr>';
+              $(tbody).append(content);
+            }
+        }
+    ).catch(err => {
+        console.log(err);
+    });
+  }
+
+
+  rederDataset4(datasetName, parentDiv) {
+    DataFrame.fromCSV('datasets/'+datasetName+'.csv').then(
+        df => {
+          //chart()
+          // var groupedDF = df.groupBy('ZIP').aggregate(group => group.count()).rename('aggregation', 'Count');
+          // chartVar = groupedDF.toCollection();
+                          //console.log(groupedDF.aggregate(group => group.count()));
+
+             //console.log(cleanDF.listColumns());
+             const selectColumns = df.select('DBN','DISTRICT','SCHOOL','PRINCIPAL','PROGRESS REPORT TYPE','SCHOOL LEVEL',
+                             'PEER INDEX','2011-2012 OVERALL GRADE','2011-2012 OVERALL SCORE',
+                             '2011-12 OVERALL PERCENTILE','2011-2012 PROGRESS CATEGORY SCORE',
+                             '2011-2012 PROGRESS GRADE','2011-2012 PERFORMANCE CATEGORY SCORE',
+                             '2011-2012 PERFORMANCE GRADE','2011-2012 ENVIRONMENT CATEGORY SCORE',
+                             '2011-2012 ENVIRONMENT GRADE','2011-2012 ADDITIONAL CREDIT',
+                             '2010-11 PROGRESS REPORT GRADE','2009-10 PROGRESS REPORT GRADE');
+
+                dataset4Data = selectColumns.toCollection();
+                 //console.log(test);
+                 var thead = '<tr>'+
+                        '<th>DBN</th><th>DISTRICT</th><th>SCHOOL</th><th>PRINCIPAL</th>'+
+                        '<th>PROGRESS REPORT TYPE</th><th>SCHOOL LEVEL</th><th>PEER INDEX</th>'+
+                        '<th>2011-2012 OVERALL GRADE</th><th>2011-2012 OVERALL SCORE</th>'+
+                        '<th>2011-12 OVERALL PERCENTILE</th><th>2011-2012 PROGRESS CATEGORY SCORE</th>'+
+                        '<th>2011-2012 PROGRESS GRADE</th><th>2011-2012 PERFORMANCE CATEGORY SCORE</th>'+
+                        '<th>2011-2012 PERFORMANCE GRADE</th><th>2011-2012 ENVIRONMENT CATEGORY SCORE</th>'+
+                        '<th>2011-2012 ENVIRONMENT GRADE</th>'+
+                        '<th>2011-2012 ADDITIONAL CREDIT</th><th>2010-11 PROGRESS REPORT GRADE</th>'+
+                        '<th>2009-10 PROGRESS REPORT GRADE</th>';
+                               '</tr>';
+                  $("#"+parentDiv+"Table thead").html(thead);
+                  var tbody = $("#"+parentDiv+"Table tbody");
+
+            for ( var i = 0; i < dataset4Data.length; i++) {
+              var content = '<tr>'+
+                              '<td>'+dataset4Data[i]["DBN"]+'</td>'+
+                              '<td>'+dataset4Data[i]["DISTRICT"]+'</td>'+
+                              '<td>'+dataset4Data[i]["SCHOOL"]+'</td>'+
+                              '<td>'+dataset4Data[i]["PRINCIPAL"]+'</td>'+
+                              '<td>'+dataset4Data[i]["PROGRESS REPORT TYPE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["SCHOOL LEVEL"]+'</td>'+
+                              '<td>'+dataset4Data[i]["PEER INDEX"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 OVERALL GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 OVERALL SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-12 OVERALL PERCENTILE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PROGRESS CATEGORY SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PROGRESS GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PERFORMANCE CATEGORY SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 PERFORMANCE GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 ENVIRONMENT CATEGORY SCORE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 ENVIRONMENT GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2011-2012 ADDITIONAL CREDIT"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2010-11 PROGRESS REPORT GRADE"]+'</td>'+
+                              '<td>'+dataset4Data[i]["2009-10 PROGRESS REPORT GRADE"]+'</td>'+
                             '</tr>';
               $(tbody).append(content);
             }
