c033af63e5b5c83892c1d3c76cfa9d6506d1f71b
patipat duangchalomnin <Patipat@dhcp22.merusouth125.iit.edu>
2017-04-27 11:56:01 -0500

    add filter

---
+++ js/filter.js
@@ -0,0 +1,10 @@
+class Filter{
+  generateColumnFilter(column,columnName){ //Collection of column header
+    var output = '<select multiple id="columnfilter-'+ columnName +'">';
+    for (var i = 0; i < column.length; i++) {
+      output += '<option value="'+ column[i] +'">'+ column[i] +'</option>';
+    }
+    output += '</select>';
+    return output;
+  }
+}
