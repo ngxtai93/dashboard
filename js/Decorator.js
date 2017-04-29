1177907b078f523ac330ca967fdf12bf8f75e7fa
patipat duangchalomnin <Patipat@dhcp155.merunorth109.iit.edu>
2017-04-29 12:16:42 -0500

    finished pats classes

--- js/Decorator.js
+++ js/Decorator.js
@@ -1,17 +1,39 @@
+/**
+* This class intention is to filter data by row name
+**/
 class DecoratorFilter{
+  /**
+  * @param {DataFrame} dataframe target dataframe object
+  * @param {String} filterName column need to be filter
+  * @param {String} filter  row value wanted to be filter
+  **/
   constructor(dataframe, filterName, filter){
     this.filterName = filterName;
     this.filter = filter;
     this.dataframe = dataframe;
   }
+
+  /**
+  * Filter dataframe by select where column value is equal to target value.
+  * @return {DataFrame} data object that is filtered
+  **/
   getFiltered(){
     var dff = this.dataframe.select(this.filterName);
     return dff.filter(row => row.get(this.filterName) === this.filter);
     //return df.groupBy(filter).aggregate(group => group.count()).rename('aggregation', 'Count');
   }
 }
-class Range{
 
+/**
+* This class intention is to filter data by using row value
+**/
+class Range{
+  /**
+  * @param {DecoratorFilter} decorated inner decorator class
+  * @param {Number} value target value need to be filter
+  * @param {String} sign Operation sign for filter
+  * @param {String} column column needed to be filter
+  **/
   constructor(decorated, value, sign, column){
     this.value = value;
     this.sign = sign;
@@ -19,6 +41,10 @@ class Range{
 
     this.decorated = decorated;
   }
+  /**
+  * Filter dataframe by append range strategy according to given sign
+  * @return {DataFrame} data object that is filtered
+  **/
   getFiltered(){
     var df = this.decorated.getFiltered()
     var collection = df.toArray();
