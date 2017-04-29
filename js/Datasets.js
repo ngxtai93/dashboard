2630c2b878568c48fe1354af1e087d610960db4d
patipat duangchalomnin <Patipat@dhcp155.merunorth109.iit.edu>
2017-04-29 12:44:22 -0500

    remove DatasetsC comments

--- js/Datasets.js
+++ js/Datasets.js
@@ -1,22 +1,9 @@
-/**
-* Base class for dataset class
-**/
 class DatasetsC {
 
-  /**
-  * Abstract plotChart()
-  **/
   plotChart() {
     throw new Error("This method must be overwritten!");
   }
 
-  /**
-  * Plot chart in to specific div
-  * @param {String} type type of chart
-  * @param {String} datasetName dataset name
-  * @param {String} divDid target div for plotting chart
-  * @param {String[]} gotDataFromFilter Prepared Data
-  **/
   createChart(type, datasetName, divID, gotDataFromFilter) {
     let chartType = this.plotChart(type, datasetName, divID, gotDataFromFilter);
 
