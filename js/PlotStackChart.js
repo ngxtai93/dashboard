fec4cc59cb5f25c9f6b4efb3798413d3db21592d
patipat duangchalomnin <Patipat@dhcp22.merusouth125.iit.edu>
2017-04-28 19:45:51 -0500

    am I change anything?

--- js/PlotStackChart.js
+++ js/PlotStackChart.js
@@ -45,6 +45,7 @@ class Stack extends ChartType {
     //   Res = result;
     // });
     setTimeout ( function () {
+
       var data1Array = [];
       var data2Array = [];
       data1Array = getStackData[0];
@@ -61,39 +62,43 @@ class Stack extends ChartType {
           borderColorArray.push("rgba(" + color1 + "," + color2 + "," + color3 + "," +Math.random()+")");
       }
 
+      var all = [];
+      var test1 = {
+          label: 'Average Student Attendance %',
+          data: data1Array,
+          backgroundColor: backgroundColorArray,
+          borderColor: borderColorArray,
+          borderWidth: 1
+      }
+      var test2 = {
+        label: 'Average Teacher Attendance',
+        data: data2Array,
+        backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 159, 64, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+    }
+    all.push(test1);
+    all.push(test2);
       var ctxStack = document.getElementById(divTestID);;
       var stackChart = new Chart(ctxStack, {
       type: 'bar',
       data: {
           labels: schoolArray,
-          datasets: [{
-              label: 'Average Student Attendance %',
-              data: data1Array,
-              backgroundColor: backgroundColorArray,
-              borderColor: borderColorArray,
-              borderWidth: 1
-          },
-            {
-              label: 'Average Teacher Attendance',
-              data: data2Array,
-              backgroundColor: [
-                  'rgba(255, 159, 64, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 99, 132, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 159, 64, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 99, 132, 1)'
-              ],
-              borderWidth: 1
-          }]
+          datasets: all
       },
       options: {
           scales: {
